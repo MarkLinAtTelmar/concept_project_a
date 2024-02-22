17de4cceb7cf9fc2a5ab3755d6a824b981ea01df
MarkLinAtTelmar <cmark@telmar.com>
2024-02-22 18:06:55 +0800

    Squashed 'share_module_x/' changes from 6348f1f..e3f02a1
    e3f02a1 add hello world
    6a1adbe Update log_utils.py
    6cb4dea Merge commit 'c54dab3758c204af4ced948ed89a1cd6ddb3476d'
    a78e96b Add test lambda
    
    git-subtree-dir: share_module_x
    git-subtree-split: e3f02a18483306a4774d7c560ad6a05f9e2f529f

--- log_utils.py
+++ log_utils.py
@@ -2,13 +2,22 @@ import logging
 
 class EngineLog():
     def __init__(self, name) -> None:
-        self.__logger = None
+        self.__logger:logging.Logger = None
         self.__logger_name = name
 
-    def get_logger(self):
+    def get_logger(self) -> logging.Logger:
         if self.__logger is None:
             self.__logger = logging.getLogger(self.__logger_name)
         return self.__logger
 
+    def print_debug_log(self, message):
+        self.get_logger().debug(message)
+
     def print_on_console(self, message):
         print(message)
+
+    def print_error_log(self, message):
+        self.get_logger().error(message)
+
+    def print_hello_world(self):
+        self.get_logger().info("hello world")
